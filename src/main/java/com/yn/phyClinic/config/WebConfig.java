5ccb04e345bd423b57d279ea1dff9c43eb9fc9cd
yna112 <yairnad111@gmail.com>
2025-05-14 10:00:36 +0300

    add webConfig class

---
+++ src/main/java/com/yn/phyClinic/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.yn.phyClinic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
+                .setCachePeriod(0);
+    }
+}
