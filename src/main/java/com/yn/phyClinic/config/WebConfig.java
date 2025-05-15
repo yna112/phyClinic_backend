94f8020708add3c31b69936ba7ea0036ee7b6185
yna112 <yairnad111@gmail.com>
2025-05-15 11:13:43 +0300

    add cors

--- src/main/java/com/yn/phyClinic/config/WebConfig.java
+++ src/main/java/com/yn/phyClinic/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.yn.phyClinic.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
                 .setCachePeriod(0);
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://phy-clinic-frontend.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
