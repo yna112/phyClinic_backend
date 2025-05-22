b7d8fb4a5e14381db956bc6cd7f8a659fc1bbd7c
yna112 <yairnad111@gmail.com>
2025-05-22 12:16:26 +0300

    add WebConfig Class

--- src/main/java/com/yn/phyClinic/config/WebConfig.java
+++ src/main/java/com/yn/phyClinic/config/WebConfig.java
@@ -1,26 +1,26 @@
-//package com.yn.phyClinic.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/swagger-ui/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
-//                .setCachePeriod(0);
-//    }
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("https://phy-clinic-frontend.vercel.app")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*");
-//        System.out.println("CORS config loaded for: https://phy-clinic-frontend.vercel.app");
-//
-//    }
-//}
+package com.yn.phyClinic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
+                .setCachePeriod(0);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://phy-clinic-frontend.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+        System.out.println("CORS config loaded for: https://phy-clinic-frontend.vercel.appmjhmnmnmnmn");
+
+    }
+}
